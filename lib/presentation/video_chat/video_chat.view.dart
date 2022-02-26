a062be5f219f888ef06409edb31733ac2f18908c
lamdangtung <lam.dt26042000@gmail.com>
2022-02-26 15:39:39 +0700

    bug: fix ui final

--- lib/presentation/video_chat/video_chat.view.dart
+++ lib/presentation/video_chat/video_chat.view.dart
@@ -57,25 +57,25 @@ class VideoChatView extends BaseView<VideoChatViewModel> {
                                 child: Assets.images.icCircleBtnChat
                                     .svg(height: 44, width: 44),
                               ),
-                              Positioned(
-                                top: -4,
-                                left: 27,
-                                child: Container(
-                                  alignment: Alignment.center,
-                                  decoration: const BoxDecoration(
-                                      color: Colors.red,
-                                      shape: BoxShape.circle),
-                                  height: 24,
-                                  width: 24,
-                                  child: const Text(
-                                    "1",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontStyle: FontStyle.normal,
-                                        fontSize: 13),
-                                  ),
-                                ),
-                              )
+                              // Positioned(
+                              //   top: -4,
+                              //   left: 27,
+                              //   child: Container(
+                              //     alignment: Alignment.center,
+                              //     decoration: const BoxDecoration(
+                              //         color: Colors.red,
+                              //         shape: BoxShape.circle),
+                              //     height: 24,
+                              //     width: 24,
+                              //     child: const Text(
+                              //       "1",
+                              //       style: TextStyle(
+                              //           color: Colors.white,
+                              //           fontStyle: FontStyle.normal,
+                              //           fontSize: 13),
+                              //     ),
+                              //   ),
+                              // )
                             ],
                           ),
                         ),

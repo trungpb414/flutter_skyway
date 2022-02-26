6f2ea91e00fbf6cdc8e346270fdc0e2a86ad9d74
lamdangtung <lam.dt26042000@gmail.com>
2022-02-26 15:36:36 +0700

    add fullscreen 3

--- lib/presentation/video_chat/video_chat.view.dart
+++ lib/presentation/video_chat/video_chat.view.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
--- lib/presentation/video_chat/widgets/video_chat_item/build_video_chat_4_people.dart
+++ lib/presentation/video_chat/widgets/video_chat_item/build_video_chat_4_people.dart
@@ -4,34 +4,71 @@ extension BuildVideoChat4People on VideoChatView {
   Widget buildVideoChat4People() {
     return Column(
       children: [
-        Expanded(
-          child: Row(
-            children: [
-              _buildItemVideoChat(
-                name: "You",
-                uiVideoChatView: _buildLocalVideo(),
-              ),
-              _buildItemVideoChat(
-                name: "User1",
-                uiVideoChatView: _createRemoteView(viewModel.peers.keys.first),
-              ),
-            ],
+        Visibility(
+          visible: viewModel.checkVisibilityByIndex(1) ||
+              viewModel.checkVisibilityByIndex(2),
+          child: Expanded(
+            child: Row(
+              children: [
+                Visibility(
+                  visible: viewModel.checkVisibilityByIndex(1),
+                  child: _buildItemVideoChat(
+                    index: 1,
+                    onDoubleTap: () {
+                      viewModel.setIndexFullScreenVideo(1);
+                    },
+                    name: "You",
+                    uiVideoChatView: _buildLocalVideo(),
+                  ),
+                ),
+                Visibility(
+                  visible: viewModel.checkVisibilityByIndex(2),
+                  child: _buildItemVideoChat(
+                    index: 2,
+                    onDoubleTap: () {
+                      viewModel.setIndexFullScreenVideo(2);
+                    },
+                    name: "User1",
+                    uiVideoChatView:
+                        _createRemoteView(viewModel.peers.keys.first),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-        Expanded(
-          child: Row(
-            children: [
-              _buildItemVideoChat(
-                name: "User2",
-                uiVideoChatView:
-                    _createRemoteView(viewModel.peers.keys.elementAt(1)),
-              ),
-              _buildItemVideoChat(
-                name: "User3",
-                uiVideoChatView:
-                    _createRemoteView(viewModel.peers.keys.elementAt(2)),
-              ),
-            ],
+        Visibility(
+          visible: viewModel.checkVisibilityByIndex(3) ||
+              viewModel.checkVisibilityByIndex(4),
+          child: Expanded(
+            child: Row(
+              children: [
+                Visibility(
+                  visible: viewModel.checkVisibilityByIndex(3),
+                  child: _buildItemVideoChat(
+                    index: 3,
+                    onDoubleTap: () {
+                      viewModel.setIndexFullScreenVideo(3);
+                    },
+                    name: "User2",
+                    uiVideoChatView:
+                        _createRemoteView(viewModel.peers.keys.elementAt(1)),
+                  ),
+                ),
+                Visibility(
+                  visible: viewModel.checkVisibilityByIndex(4),
+                  child: _buildItemVideoChat(
+                    index: 4,
+                    onDoubleTap: () {
+                      viewModel.setIndexFullScreenVideo(4);
+                    },
+                    name: "User3",
+                    uiVideoChatView:
+                        _createRemoteView(viewModel.peers.keys.elementAt(2)),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ],
@@ -39,69 +76,88 @@ extension BuildVideoChat4People on VideoChatView {
   }
 
   Widget _buildItemVideoChat(
-      {
-        bool isLoading = false,
-        required String name, required Widget uiVideoChatView}) {
+      {required int index,
+      required GestureDoubleTapCallback onDoubleTap,
+      bool isLoading = false,
+      required String name,
+      required Widget uiVideoChatView}) {
     return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(4),
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: uiVideoChatView,
-            ),
-            Container(
-              decoration: BoxDecoration(
+      child: GestureDetector(
+        onDoubleTap: onDoubleTap,
+        child: Padding(
+          padding: const EdgeInsets.all(4),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              ClipRRect(
                 borderRadius: BorderRadius.circular(12),
-                color: Colors.black.withOpacity(0.6),
+                child: uiVideoChatView,
               ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 12),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(name)
-                        .defaultStyle()
-                        .color(Colors.white)
-                        .fontStyle(FontStyle.normal)
-                        .fontWeight(FontWeight.w500)
-                        .fontSize(14),
-                    const SizedBox(
-                      width: 6,
-                    ),
-                    Assets.images.icMicUnmute.svg(height: 16, width: 16),
-                  ],
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.black.withOpacity(0.6),
                 ),
               ),
-            ),
-            Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 12,
-                  right: 8,
-                ),
-                child: Assets.images.icDots.svg(height: 24, width: 24),
-              ),
-            ),
-            Visibility(
-              visible: isLoading,
-              child: const Align(
-                alignment: Alignment.center,
+              Align(
+                alignment: Alignment.bottomCenter,
                 child: Padding(
-                  padding: EdgeInsets.all(8),
-                  child: CircularProgressIndicator(
-                    value: null,
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(name)
+                          .defaultStyle()
+                          .color(Colors.white)
+                          .fontStyle(FontStyle.normal)
+                          .fontWeight(FontWeight.w500)
+                          .fontSize(14),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Assets.images.icMicUnmute.svg(height: 16, width: 16),
+                    ],
                   ),
                 ),
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 12,
+                    right: 8,
+                  ),
+                  child: Assets.images.icDots.svg(height: 24, width: 24),
+                ),
+              ),
+              Visibility(
+                visible: viewModel.isLoading,
+                child: const Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: CircularProgressIndicator(
+                      value: null,
+                    ),
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: viewModel.checkVisibilityByIndex(index) &&
+                    viewModel.isFullScreenEnabled,
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: GestureDetector(
+                    onTap: viewModel.disableFullscreenVideoMode,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Icon(Icons.close, size: 48, color: Colors.white),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
